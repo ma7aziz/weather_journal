7cd27815c0867118e79c72bd5c47f86e4dde738b
ma7aziz <45315298+ma7aziz@users.noreply.github.com>
2021-09-09 12:09:09 +0200

    initial commit && Setup express and local server

---
+++ server.js
@@ -0,0 +1,26 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require("express");
+const app = express();
+const port = 3000;
+
+//  require body-parser
+const bodyParser = require("body-parser");
+// Start up an instance of app
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+
+// Initialize the main project folder
+app.use(express.static("website"));
+
+// Setup Server
+app.listen(port, () => {
+  console.log(`Server is listining on port ${port} !`);
+});
